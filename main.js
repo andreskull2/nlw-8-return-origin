efa166cee55ddbc484d9489bd337a89714489044
andreskull2 <andreskull1@gmail.com>
2025-02-21 10:42:30 -0300

    aula 3

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-function onScroll() {
-    if (scrollY > 0) {
-        navigation.classList.add('scroll')
-    } else {
-        navigation.classList.remove('scroll')
-    }
-}
+/*=== TROCA COR DO MENU =================================*/
+const navigation = document.querySelector('#navigation'); // Substitua pelo seletor correto
 
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+        navigation.classList.add('scroll');
+    } else {
+        navigation.classList.remove('scroll');
+    }
+});
+
+/*=== MENU-EXPANDED =================================*/
 function openMenu() {
     document.body.classList.add('menu-expanded')
-  }
+}
   
-  function closeMenu() {
+function closeMenu() {
     document.body.classList.remove('menu-expanded')
-  }
+}
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+    #home,
+    #home img,
+    #home .stats,
+    #services,
+    #services header,
+    #services .card,
+    #about,
+    #about header,
+    #about .content
+    `)
   
